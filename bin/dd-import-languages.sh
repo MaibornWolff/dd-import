5190655f212257b8348539089def5d507dba9d35
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-08-17 14:36:56 +0200

    set PYTHONPATH

--- bin/dd-import-languages.sh
+++ bin/dd-import-languages.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export PYTHONPATH="${PYTHONPATH}:/usr/local/dd-import"
 python -m dd_import.dd_import_languages
--- bin/dd-reimport-findings.sh
+++ bin/dd-reimport-findings.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export PYTHONPATH="${PYTHONPATH}:/usr/local/dd-import"
 python -m dd_import.dd_reimport_findings
