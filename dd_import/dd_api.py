a4c7db115eb247eb9776ba958a858e587c5aee99
satandyh <8116964+satandyh@users.noreply.github.com>
2022-04-13 17:16:41 +0300

    Update dd_api.py
    added skip_tls option for self signed certificates

--- dd_import/dd_api.py
+++ dd_import/dd_api.py
@@ -25,7 +25,8 @@ class Api:
         payload = {'name': self.environment.product_type_name}
         r = requests.get(self.product_type_url,
                          headers=self.headers,
-                         params=payload)
+                         params=payload,
+                         verify=self.environment.skip_tls)
         r.raise_for_status()
         product_type_data = json.loads(r.text)
         for product_type in product_type_data.get('results', []):
@@ -40,7 +41,8 @@ class Api:
                    'prod_type': product_type}
         r = requests.get(self.product_url,
                          headers=self.headers,
-                         params=payload)
+                         params=payload,
+                         verify=self.environment.skip_tls)
         r.raise_for_status()
         product_data = json.loads(r.text)
         for product in product_data.get('results', []):
@@ -56,7 +58,8 @@ class Api:
                    'prod_type': product_type}
         r = requests.post(self.product_url,
                           headers=self.headers,
-                          data=json.dumps(payload))
+                          data=json.dumps(payload),
+                          verify=self.environment.skip_tls)
         r.raise_for_status()
         product_data = json.loads(r.text)
         print('New product,        id: ', product_data['id'])
@@ -67,7 +70,8 @@ class Api:
                    'product': product}
         r = requests.get(self.engagement_url,
                          headers=self.headers,
-                         params=payload)
+                         params=payload,
+                         verify=self.environment.skip_tls)
         r.raise_for_status()
         engagement_data = json.loads(r.text)
         for engagement in engagement_data.get('results', []):
@@ -86,7 +90,8 @@ class Api:
                    'status': 'In Progress'}
         r = requests.post(self.engagement_url,
                           headers=self.headers,
-                          data=json.dumps(payload))
+                          data=json.dumps(payload),
+                          verify=self.environment.skip_tls)
         r.raise_for_status()
         engagement_data = json.loads(r.text)
         print('New engagement,     id: ', engagement_data['id'])
@@ -101,7 +106,8 @@ class Api:
                        'branch_tag': self.environment.branch_tag}
             r = requests.patch(self.engagement_url + str(engagement) + '/',
                                headers=self.headers,
-                               data=json.dumps(payload))
+                               data=json.dumps(payload),
+                               verify=self.environment.skip_tls)
             r.raise_for_status()
 
     def get_test(self, engagement):
@@ -109,7 +115,8 @@ class Api:
                    'engagement': engagement}
         r = requests.get(self.test_url,
                          headers=self.headers,
-                         params=payload)
+                         params=payload,
+                         verify=self.environment.skip_tls)
         r.raise_for_status()
         test_data = json.loads(r.text)
         for test in test_data.get('results', []):
@@ -130,7 +137,8 @@ class Api:
                    'test_type': self.get_test_type()}
         r = requests.post(self.test_url,
                           headers=self.headers,
-                          data=json.dumps(payload))
+                          data=json.dumps(payload),
+                          verify=self.environment.skip_tls)
         r.raise_for_status()
         test_data = json.loads(r.text)
         print('New test,           id: ', test_data['id'])
@@ -140,7 +148,8 @@ class Api:
         payload = {'name': self.environment.test_type_name}
         r = requests.get(self.test_type_url,
                          headers=self.headers,
-                         params=payload)
+                         params=payload,
+                         verify=self.environment.skip_tls)
         r.raise_for_status()
         test_type_data = json.loads(r.text)
         for test_type in test_type_data.get('results', []):
@@ -173,11 +182,13 @@ class Api:
             response = requests.post(self.reimport_scan_url,
                                      headers=self.headers_without_json,
                                      data=payload,
-                                     files=files)
+                                     files=files,
+                                     verify=self.environment.skip_tls)
         else:
             response = requests.post(self.reimport_scan_url,
                                      headers=self.headers_without_json,
-                                     data=payload)
+                                     data=payload,
+                                     verify=self.environment.skip_tls)
 
         response.raise_for_status()
 
@@ -193,7 +204,8 @@ class Api:
         response = requests.post(self.import_languages_url,
                                  headers=self.headers_without_json,
                                  data=payload,
-                                 files=files)
+                                 files=files,
+                                 verify=self.environment.skip_tls)
         response.raise_for_status()
 
         print()
