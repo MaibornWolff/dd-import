e138ca3d71c7f218dc7e080c2d862284d9a6c973
jdfresser <60644781+jdfresser@users.noreply.github.com>
2023-10-04 07:05:55 +0200

    Add support for source_code_management_uri while creating engagements (#110)
    Co-authored-by: Jean-Damien <jdfresser@qosenergy.com>

--- unittests/test_api.py
+++ unittests/test_api.py
@@ -235,6 +235,28 @@ class TestApi(TestCase):
         response.raise_for_status.assert_called_once()
 
     @patch('dd_import.environment.Environment')
+    @patch('requests.post')
+    @patch.dict('os.environ', {'DD_URL': 'https://example.com',
+                               'DD_API_KEY': 'api_key',
+                               'DD_ENGAGEMENT_NAME': 'engagement',
+                               'DD_SOURCE_CODE_MANAGEMENT_URI': 'https://github.com/MyOrg/MyProject/tree/main'})
+    def test_new_engagement_with_source_code_management_uri(self, mockPost, mockEnv):
+        response = Mock(spec=Response)
+        response.status_code = 200
+        response.text = '{\"id\": 3}'
+        mockPost.return_value = response
+
+        api = Api()
+        id = api.new_engagement(self.product_id)
+
+        self.assertEqual(id, self.engagement_id)
+        today = datetime.date.today().isoformat()
+        url = 'https://example.com/api/v2/engagements/'
+        payload = f'{{"name": "engagement", "product": 2, "target_start": "{today}", "target_end": "2999-12-31", "engagement_type": "CI/CD", "status": "In Progress", "source_code_management_uri": "https://github.com/MyOrg/MyProject/tree/main"}}'
+        mockPost.assert_called_once_with(url, headers=self.header, data=payload, verify=True)
+        response.raise_for_status.assert_called_once()
+
+    @patch('dd_import.environment.Environment')
     @patch('requests.patch')
     @patch.dict('os.environ', {'DD_URL': 'https://example.com',
                                'DD_API_KEY': 'api_key',
